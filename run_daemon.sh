5229205f4e5670cdf66491b8a7fb32e06dfe083f
Geoff Lawler <glawler@tislabs.com>
2018-03-16 16:35:54 -0700

    run_daemon.sh now understands "restart" as a command.

--- run_daemon.sh
+++ run_daemon.sh
@@ -10,6 +10,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ $1 == stop ]]; then 
     pkill -f agent_server.py
     exit 0
+elif [[ $1 == restart ]]; then
+    pkill -f agent_server.py
 fi
 
 PYTHONPATH=${DIR}/dgrpc:${DIR}/dgrpc/pb ${DIR}/bin/agent_server.py -v > /tmp/dgrpc.log 2>&1 &
