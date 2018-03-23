c91c5937f65984c2f612eb358e775204d7583e6e
Geoff Lawler <glawler@tislabs.com>
2018-03-23 13:49:13 -0700

    added iperf3 source and build script

---
+++ sources/iperf3_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+trap exit ERR
+
+src_dir=$1
+tmp_dir=$2
+tar_file=iperf-3.1.3.tgz
+
+cp -v ${src_dir}/${tar_file} ${tmp_dir}
+cd $tmp_dir
+tar zxf ${tar_file}
+cd iperf-3.1.3
+sudo make install
+
+exit 0
