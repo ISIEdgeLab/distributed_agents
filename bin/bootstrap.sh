ca022d89b59b03f88bb9a25eddc8e13e8c1f6781
Geoff Lawler <glawler@tislabs.com>
2018-03-26 12:53:09 -0700

    append to log, do not overwrite with each echo. correctly set working dir - bootstrap will now always install out of its own directory/repo.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-DGRPC_DIR=/users/glawler/src/distributed_agents
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+DGRPC_DIR=${DGRPC_DIR:-${CURDIR}}
 INSTALL_LOG=/tmp/dgprc-install.log
 
 if [[ $(id -u) -ne 0 ]]; then 
@@ -8,10 +9,13 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 fi
 
+echo Install started at $(date) &> ${INSTALL_LOG}
+echo Installing from ${DGRPC_DIR} &>> ${INSTALL_LOG}
+
 # install python package
-pushd ${DGRPC_DIR} &> ${INSTALL_LOG}
-python3 ./setup.py install &> ${INSTALL_LOG}
-popd ${DGRPC_DIR} &> ${INSTALL_LOG}
+pushd ${DGRPC_DIR} &>> ${INSTALL_LOG}
+python3 setup.py install &>> ${INSTALL_LOG}
+popd &>> ${INSTALL_LOG}
 
 # now setup to run as a service/daemon
 if [[ ! -e /usr/bin/lsb_release ]]; then 
@@ -27,18 +31,18 @@ case ${LSB_ID} in
             14.*)
                 echo Installing services as an upstart service.
                 if [[ -e /etc/init.d/dgprc ]]; then 
-                    update-rc.d -f dgrpc remove
+                    update-rc.d -f dgrpc remove &>> ${INSTALL_LOG}
                 fi
-                cp ${DGRPC_DIR}/etc/dgrpc.upstart /etc/init.d/dgrpc;
-                chmod 755 /etc/init.d/dgrpc
-                update-rc.d dgrpc defaults 99
-                /etc/init.d/dgrpc start
-                /etc/init.d/dgrpc status
+                cp ${DGRPC_DIR}/etc/dgrpc.upstart /etc/init.d/dgrpc &>> ${INSTALL_LOG};
+                chmod 755 /etc/init.d/dgrpc &>> ${INSTALL_LOG} ;
+                update-rc.d dgrpc defaults 99 &>> ${INSTALL_LOG} ;
+                /etc/init.d/dgrpc start &>> ${INSTALL_LOG} ;
+                /etc/init.d/dgrpc status &>> ${INSTALL_LOG} ;
                 ;;
             16.*)
                 echo Installing service as a systemd service. 
-                cp ${DGRPC_DIR}/etc/dgrpc.service /lib/systemd/system ;
-                service dgrpc start ;
+                cp ${DGRPC_DIR}/etc/dgrpc.service /lib/systemd/system &>> ${INSTALL_LOG} ;
+                service dgrpc start &>> ${INSTALL_LOG} ;
                 ;;
             *)
                 echo Unsupported version of ${LSB_ID}, ${LSB_REL}. Please install service by hand.
